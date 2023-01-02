88a3db046fb5832a86dca529fd3a23091b9ee02c
leonardo <84354110+lrivas3@users.noreply.github.com>
2023-01-01 21:15:27 -0600

    Commit before merge from fetched

--- BashconS4vitar/bandit12/decompresor.sh
+++ BashconS4vitar/bandit12/decompresor.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-name_compressed=$(7z l data.gzip | grep 'Name' -A 2 | tail -n 1 | awk 'NF{print $NF}')
-
-echo $name_compressed
+name_compressed=$(7z l content.gzip | grep 'Name' -A 2 | tail -n 1 | awk 'NF{print $NF}')
+7z x content.gzip > /dev/null 2>&1
